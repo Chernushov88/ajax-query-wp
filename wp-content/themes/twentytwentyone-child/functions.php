ccbc1cb9d8d8d7973a898c91ff0f723cd89d6cf0
Serhii Chernushov <sergey.chernyshov88@gmail.com>
2026-03-10 15:02:14 +0200

    chore: add UI validation.

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -3,11 +3,11 @@ function twentytwentyone_child_enqueue_styles() {
 	wp_enqueue_style(
 		'twentytwentyone-child-style',
 		get_stylesheet_uri(),
+		array('twenty-twenty-one-style'),
 		wp_get_theme()->get('Version')
 	);
 }
 add_action('wp_enqueue_scripts', 'twentytwentyone_child_enqueue_styles');
-
 
 function twentytwentyone_child_enqueue_scripts() {
     wp_register_script(
--- wp-content/themes/twentytwentyone-child/js/ajax.js
+++ wp-content/themes/twentytwentyone-child/js/ajax.js
@@ -1,39 +1,53 @@
-const form = document.getElementById("true-get-id");
-const result = document.getElementById("result");
+/********** AJAX in JavaScript only **********/
+// const form = document.getElementById("true-get-id");
+// const result = document.getElementById("result");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  form.querySelector("button").textContent = "Sending a request...";
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   form.querySelector("button").textContent = "Sending a request...";
 
-  const request = new XMLHttpRequest();
-  request.open("POST", serhii.ajax_url, true);
-  request.send(new FormData(form));
-  request.onload = function () {
-    const resp = this.response;
-    result.innerHTML = resp;
-    form.querySelector("button").textContent = "Find out the title";
-  };
-  request.send();
-});
-// jQuery(function ($) {
-//   $("#true-get-id").submit(function (event) {
-// 		event.preventDefault();
-
-//     const form = $(this);
-
-//     $.ajax({
-//       type: "POST",
-//       url: serhii.ajax_url,
-//       data: form.serialize(),
-//       beforeSend: function (xhr) {
-//         form.find("button").text("Loading...");
-//       },
-//       success: function (data) {
-//         $("#result").html(data);
-//         form.find("button").text("Submit");
-//       },
-//     });
-
-//     return false;
-//   });
+//   const request = new XMLHttpRequest();
+//   request.open("POST", serhii.ajax_url, true);
+//   request.send(new FormData(form));
+//   request.onload = function () {
+//     const resp = this.response;
+//     result.innerHTML = resp;
+//     form.querySelector("button").textContent = "Find out the title";
+//   };
+//   request.send();
 // });
+
+jQuery(function ($) {
+  $("#true-get-id").submit(function (event) {
+    event.preventDefault();
+
+    const form = $(this);
+    const postIdInput = form.find('input[name="post_id"]');
+    const postIdValue = postIdInput.val().trim();
+
+    postIdInput.removeClass("error");
+    $("#result").removeClass("text-error");
+    if (!postIdValue || isNaN(postIdValue) || parseInt(postIdValue) <= 0) {
+      $("#result")
+        .html("Please enter a valid numeric ID.")
+        .addClass("text-error");
+      postIdInput.addClass("error");
+      return false;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: serhii.ajax_url,
+      data: form.serialize(),
+      beforeSend: function (xhr) {
+        form.find("button").text("Loading...");
+      },
+      success: function (data) {
+        $("#result").html(data);
+        form.find("button").text("Submit");
+      },
+    });
+
+    return false;
+  });
+});
